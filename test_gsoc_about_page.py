1b0ff04b997ebde3a199d4f78086e5e66258fb55
Syed Armani <dce3062@gmail.com>
2012-06-23 17:57:05 +0530

    GSOC About Page

---
+++ test_gsoc_about_page.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python2.5
+#
+# Copyright 2012 the Melange authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+""" This test case test the functionality of GSoC About Page.
+"""
+
+import unittest
+
+from melange_functional_actions import *
+
+class AboutPageTest(unittest.TestCase, FunctionalTestCase):
+  
+  def setUp(self):
+    FunctionalTestCase.__init__(self)
+    self.setup()
+    self.getParameters("/home/syed/Desktop/testdata_melange.xls", "GSOC_About_Test")
+
+  def test_About_Page(self):
+    #Test Url, Change it according to your local dev environment.
+    self.Browser.get(self.obj_id['Url'])
+
+    #Check for the correct browser title.
+    self.assertIn("Google Summer of Code", self.Browser.title)
+
+    #Click on About Page.
+    self.clickOn("xpath", "About")
+
+    #Check for the browser title.
+    self.wait(5)
+    self.assertIn("About Google Summer of Code", self.Browser.title)
+
+    #Assert message about GSoC.
+    self.assertText("About_GSoC")
+
+    self.assertLink("Timeline")
+    self.assertLink("Frequently Asked Questions")
+    self.assertLink("Internet Relay Chat (IRC)")
+    self.assertLink("mailing lists")
+    self.assertLink("social networking sites")
+    self.assertLink("Google Open Source Blog")
+
+
+def tearDown(self):
+    self.teardown()
+
+
+if __name__ == "__main__":
+  unittest.main()    
+
+    
