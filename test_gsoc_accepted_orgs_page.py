81554ded625e3ab7135b5be6e35938882abf70bc
Syed Armani <syedarmani08@gmail.com>
2012-07-01 21:23:36 +0530

    GSoC Accepted Organisation Page.

--- test_gsoc_accepted_orgs_page.py
+++ test_gsoc_accepted_orgs_page.py
@@ -30,7 +30,6 @@ class AcceptedOrganisationsTest(unittest.TestCase, FunctionalTestCase):
     self.getParameters('./tests/functional/testdata_melange.xls', 'Accepted_Orgs')    
      
   def testForAcceptedOrgs(self):
-
     #Test Url, Change it according to your local dev environment.
     self.Browser.get(self.obj_id['Url'])
 
