8cf1ed4426d1562bfff3a255815aa620dc0d9ce1
Syed Armani <syedarmani08@gmail.com>
2012-07-02 18:19:38 +0530

    Base Module.

--- melange_functional_actions.py
+++ melange_functional_actions.py
@@ -17,10 +17,10 @@
 """Base module for writing functional test scripts.
 """
 
+import logging
 import random
 import string
 import time
-import logging
 
 from selenium import webdriver
 from selenium.common import exceptions
@@ -31,7 +31,6 @@ class FunctionalTestCase(object):
   """ Base Class for all the Melange Functional Tests.
       Contains actions which will be used in writing Test scripts.
   """
-
   def __init__(self):
     self.obj_id = {}
     self.obj_val = {}
@@ -71,7 +70,7 @@ class FunctionalTestCase(object):
     print "waiting for page to load for %s seconds " % sec
     time.sleep(sec)
 
-  def writeTextField(self, id_type="", element=""):
+  def writeTextField(self, id_type=None, element=None):
     """ Write text field in a form.
 
     Args:
@@ -85,9 +84,9 @@ class FunctionalTestCase(object):
       self.Browser.find_element_by_xpath(self.\
                            obj_id[element]).send_keys(self.obj_val[element])
     else:
-      raise NoSuchElementException 
+      raise TypeError 
 
-  def toggleCheckBox(self, id_type="", chk_box=""):
+  def toggleCheckBox(self, id_type=None, chk_box=None):
     """ Toggle a check box.
 
     Args:
@@ -99,9 +98,9 @@ class FunctionalTestCase(object):
     elif id_type == "xpath":
       self.Browser.find_element_by_xpath(self.obj_id[chk_box]).click()
     else:
-      raise NoSuchElementException
+      raise TypeError
 
-  def setDropDownList(self, select_opt=""):
+  def setDropDownList(self, select_opt=None):
     """ Selects one option from the drop down list.
 
     Args:
@@ -114,7 +113,7 @@ class FunctionalTestCase(object):
       if (option.get_attribute("value") == self.obj_val[select_opt]):
         option.click()
     
-  def waitAndEnterText(self, sec, id_type="", element=""):
+  def waitAndEnterText(self, sec, id_type=None, element=None):
     """ Wait and enter text in a particular field.
 
     Args:
@@ -130,20 +129,20 @@ class FunctionalTestCase(object):
       self.Browser.find_element_by_xpath(self.obj_id[element]).send_keys\
                                                         (self.obj_val[element])
     else:
-      raise NoSuchElementException   
+      raise TypeError   
 
-  def clearFieldAssertMessageAndEnterData(self, error_element , element=""):
+  def clearFieldAssertMessageEnterData(self, error_element=None, element=None):
     """Assert the error message , clear the input field and enter a new value.
 
     Args:
-      erro_element: It is the element which is showing error message.
+      error_element: It is the element which is showing error message.
       element: The correct value for the input field.                 
     """
     self.assertTextIn(error_element)
     self.clearField("xpath", element)
     self.writeTextField("xpath", element)
  
-  def clearField(self, id_type="", clear_element=""):
+  def clearField(self, id_type=None, clear_element=None):
     """ Wait and clear a particular field.
 
     Args:
@@ -155,9 +154,9 @@ class FunctionalTestCase(object):
     elif id_type == "xpath":
       self.Browser.find_element_by_xpath(self.obj_id[clear_element]).clear()
     else:
-      raise NoSuchElementException   
+      raise TypeError   
  
-  def clickOn(self, id_type="", click_element=""):
+  def clickOn(self, id_type=None, click_element=None):
     """ Click on the specified element.
 
     Args:
@@ -169,7 +168,7 @@ class FunctionalTestCase(object):
     elif id_type == "xpath":
       self.Browser.find_element_by_xpath(self.obj_id[click_element]).click()
     else:
-      raise NoSuchElementException
+      raise TypeError
 
   def assertError(self, msg):
     """Print the message and raise assertion error.
@@ -177,22 +176,21 @@ class FunctionalTestCase(object):
     Args:
       msg: The message which should be printed.  
     """
-    print msg
     raise AssertionError(msg)
 
-  def assertLink(self, link_text=""):
+  def assertLink(self, link_text=None):
     """Assert if a link is there.
 
     Args:
       link_text: The link which will be tested.  
     """
     try:
-      link = self.Browser.find_element_by_link_text(link_text)      
-    except NoSuchElementException as e :
+      self.Browser.find_element_by_link_text(link_text)      
+    except exceptions.NoSuchElementException:
       msg = "The text %s is not part of a Link" % link_text
       self.assertError(msg)
 
-  def assertText(self, text_element=""):
+  def assertText(self, text_element=None):
     """Assert a particular text.
 
     Args:
@@ -207,11 +205,11 @@ class FunctionalTestCase(object):
                                             % (self.obj_val[text_element], txt)
         self.assertError(msg)
 
-  def assertMessageAndEnterText(self, error_element="", input_field=""):
+  def assertMessageAndEnterText(self, error_element=None, input_field=None):
     """Assert a message and enter value in the text field.
 
     Args:
-      error_element : the error message from the application which will be checked.
+      error_element : error message from the application which will be checked.
       input_field : input box in which a value will be entered.
     """
     self.assertText(error_element)
@@ -230,32 +228,31 @@ class FunctionalTestCase(object):
         msg = "Element %s has no text %s " % (text_element, text_msg)
         self.assertError(msg)
     if text_msg not in self.obj_val[text_element]:
-        msg = "Element text should be %s.  It is %s." % (self.obj_val[\
-                                                         text_element], text_msg)
+        msg = "Element text should be %s. It is %s." % (self.obj_val[\
+                                                        text_element], text_msg)
         self.assertError(msg)
     if text_msg in self.obj_val[text_element]:
       return True
 
-  def isElementDisplayed(self, sec, element_displayed=""):
+  def isElementDisplayed(self, sec, element_displayed=None):
     """ Wait and check if a particular element is displayed.
 
     Args:
       sec: Number of seconds script should wait.
       element_displayed: A particular element which we want to check if it is 
-                         displayed.Return True if it is present else return false.
-                         if it is not displayed just pass and continue the execution.
+      displayed.Return True if it is present else return false. if it is not 
+      displayed just pass and continue the execution.
     """   
     self.wait(sec)
     try:
-      if self.Browser.find_element_by_xpath\
-                                 (self.obj_id[element_displayed]).is_displayed():
-        return True
-      else:
-        return False
+      if self.Browser.find_element_by_xpath(self.obj_id\
+                                            [element_displayed]).is_displayed():
+        return True        
     except:
+      return False
       pass
 
-  def fillRandomValue(self, element=""):
+  def fillRandomValue(self, element=None):
     """ It takes a value , add random string at the end and fill it in the form.
 
     Args:
@@ -269,7 +266,7 @@ class FunctionalTestCase(object):
     self.clearField("xpath", element)
     self.Browser.find_element_by_xpath(self.obj_id[element]).send_keys(val)
 
-  def waitAndClick(self, sec, id_type="", click_element=""):
+  def waitAndClick(self, sec, id_type=None, click_element=None):
     """ wait and click on a particular element.
 
     Args:
@@ -283,9 +280,9 @@ class FunctionalTestCase(object):
     elif id_type == "xpath":
       self.Browser.find_element_by_xpath(self.obj_id[click_element]).click()
     else:
-      raise NoSuchElementException
+      raise TypeError
 
-  def checkRegistrationSuccess(self, flash_message=""):
+  def checkRegistrationSuccess(self, flash_message=None):
     """Check Message from the melange if student data is saved successfully.
 
     Args:
@@ -299,7 +296,7 @@ class FunctionalTestCase(object):
       if text == "Data saved successfully.":
         pass
     
-  def takeScreenshot(self, path=""):
+  def takeScreenshot(self, path=None):
     """Take screenshot.
     """
     self.Browser.save_screenshot(path)
